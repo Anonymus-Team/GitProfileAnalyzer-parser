56e69bb42570b4c4c0e3424a0ec9c699e63500c0
Evgeny <sev.20@uni-dubna.ru>
2023-04-03 23:21:44 +0300

    feat(get_resumes): rewrite and make parse 100 items from one page
    Rewrite was itself to make support more items on page

--- get_resumes.sh
+++ get_resumes.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-URL='https://dubna.hh.ru/search/resume?text=github.com%2F.%2B&area=1&ored_clusters=true&order_by=relevance&search_period=0&logic=normal&pos=full_text&exp_period=all_time&label=only_with_salary&page='
+SEARCH_URL='https://dubna.hh.ru/search/resume?text=github.com%2F.%2B&area=1&ored_clusters=true&order_by=relevance&search_period=0&logic=normal&pos=full_text&exp_period=all_time&label=only_with_salary&items_on_page=100&page='
+
+export RESUME_URL='https://dubna.hh.ru/resume/'
 
 get_resumes_from_page() {
     pagenum="$1"
-    curl -s "$URL$pagenum" \
-        | grep -Po 'href="\K/resume/.*?\?query=' \
-        | awk '{printf "https://hh.ru" substr($1, 1, length($1)-7) "\n"}'
-    # P.S. 7 in awk substr is length of regexp part "?query="
+    curl -s "$SEARCH_URL$pagenum" \
+        | grep -Po '"resumes": \K\[.*\], "settings":' \
+        | head -c -14 \
+        | jq -r '@text "\(env.RESUME_URL)\(.[]._attributes.hash)"'
+    # P.S. 14 in head is length of part from regex: ', "settings":'
+    # P.P.S. there, in jq, we already have most of the metadata, but not all of
+    # them, so we don't have fields which contains github links. So sad
 }
 
 get_resumes_from_page 0
