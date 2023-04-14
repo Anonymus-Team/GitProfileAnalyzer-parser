6adc85bdaf3e5b9763a14dfba1ff8ff601963bea
Hassel2 <hasselprassel@mail.ru>
2023-04-14 10:44:52 +0300

    Add get_reps.sh

---
+++ get_reps.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/sh
+
+while IFS= read -r line; do 
+    URL=$(echo $line | jq -c .git);
+
+    # need a substring cos jq outputs url like "github.com/ExampleUsr",
+    # so we need to truncate github.com and all the quotes
+    NICK=$(echo ${URL:12:-1});
+
+    # GHP_TOKEN is an enviroment variable with your github access token see: 
+    # https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token
+    
+    # Script works perfectly even without token, but there is limit for requests without Authorization
+    REPS=$(curl -Ls \
+        -H "Accept: application/vnd.github+json" \
+        -H "Authorization: Bearer $GHP_TOKEN" \
+        -H "X-GitHub-Api-Version: 2022-11-28" \
+        https://api.github.com/users/{$NICK}/repos \
+        | jq -c '[.[].html_url]');
+    echo $line | jq -c --arg reps "$REPS" '.git = $reps';
+done < $1;
+
+# TODO: check if account exist
