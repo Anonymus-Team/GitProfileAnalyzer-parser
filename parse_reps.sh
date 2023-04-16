2bbc1dac2f508a861189b81cc7f657518a0c5f01
Hassel2 <hasselprassel@mail.ru>
2023-04-16 13:16:06 +0300

    feat(parse_reps.sh): use /tmp instead local tmp folder

--- parse_reps.sh
+++ parse_reps.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
 
+CURRENT_FOLDER=$(pwd)
+DATA=$(pwd)/$1
+DIFFS_FOLDER=$(pwd)/diffs
+TMP_FOLDER=/tmp/GitProfileAnalyzer
+mkdir $TMP_FOLDER;
+
+cd $TMP_FOLDER;
+
 while IFS= read -r line; do 
     HASH=$(echo $line | jq -r '.id');
 
-    touch ./diffs/$HASH;
+    touch $DIFFS_FOLDER/$HASH;
 
     URLS=( $(echo $line | jq -r '.github | join("\n")') );
     for repo in ${URLS[@]}; do 
         # clone only .git folder and one branch only
-        git clone $repo ./tmp --single-branch --no-tags --bare; 
-        cd ./tmp;
+        git clone $repo $TMP_FOLDER --single-branch --no-tags --bare; 
 
         # get an array of all commits id and reverse it
         HISTORY=( $(git log --pretty=format:"%h") );
         HISTORY=( $(printf '%s\n' "${HISTORY[@]}" | tac | tr '\n' ' '; echo) );
 
         for commit in ${HISTORY[@]}; do
-            git show $cur_commit >> ../diffs/$HASH;
+            git show $cur_commit >> $DIFFS_FOLDER/$HASH;
         done;
 
         rm -rf *;
 
-        cd ..;
     done;
-done < $1;
+done < $DATA;
+
+cd $CURRENT_FOLDER;
